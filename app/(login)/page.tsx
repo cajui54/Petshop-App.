1c5b493c1a05ac1933897c0df307fe3e021a79de
cajui54 <cajui54@gmail.com>
2025-02-20 19:33:01 -0300

    style: fixe bugs of styles

--- app/(login)/page.tsx
+++ app/(login)/page.tsx
@@ -7,15 +7,22 @@ import {
   TitlesWithLogo,
 } from "../_components/titles-with-logo";
 import ButtonLogin from "../_components/button-login";
+import imgPug from "../_assets/images/dog-pug.png";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <div className="bg-black flex justify-center items-center w-full h-full">
-      <div className="w-[85%] sm:w-[400px] bg-neutral-900 rounded-lg border shadow-md shadow-pink-300 border-pink-500">
+    <div className="flex h-full w-full items-center justify-center bg-black">
+      <div className="w-[85%] rounded-lg border border-pink-500 bg-neutral-900 shadow-md shadow-pink-300 sm:w-[400px]">
+        <div className="relative mx-auto mt-1 h-[200px] w-[99%] overflow-hidden">
+          <div className="gradientBg absolute z-10 h-full w-full"></div>
+          <Image
+            src={imgPug}
+            alt="4 dogs pug"
+            className="mx-auto -mt-28 w-[350px]"
+          />
+        </div>
         <TitlesWithLogo>
-          <IconTitleComponent>
-            <SiPug />
-          </IconTitleComponent>
           <TitleComponent>Petshop App.</TitleComponent>
           <SubtitleComponent>Banho e Tosa</SubtitleComponent>
         </TitlesWithLogo>
